7a78774c21a2c296790c78f46e8af2fe85d03d55
notsapinho <rafaelalibibi@gmail.com>
2022-07-06 12:45:20 -0300

    Refactor

--- index.jsx
+++ index.jsx
@@ -3,6 +3,8 @@ const {
 	React,
 	i18n: { Messages }
 } = require("powercord/webpack");
+
+const { injectContextMenu } = require("powercord/util");
 const { inject, uninject } = require("powercord/injector");
 const { Plugin } = require("powercord/entities");
 
@@ -16,69 +18,33 @@ module.exports = class MuteFolder extends Plugin {
 	startPlugin() {
 		powercord.api.i18n.loadAllStrings(i18n);
 
-		this.patchContextMenu("GuildFolderContextMenu", (GuildFolderContextMenu) => {
-			inject("mute-folder-context-menu-lazy", GuildFolderContextMenu, "default", (args, res) => {
-				const currFolder = getGuildFolderById(args[0].folderId);
+		injectContextMenu("mute-folder-context-menu-lazy", "GuildFolderContextMenu", (args, res) => {
+			const currFolder = getGuildFolderById(args[0].folderId);
 
-				const unmutedGuilds = currFolder.guildIds.filter((g) => !isMuted(g));
-				const mutedGuilds = currFolder.guildIds.filter((g) => isMuted(g));
+			const unmutedGuilds = currFolder.guildIds.filter((g) => !isMuted(g));
+			const mutedGuilds = currFolder.guildIds.filter((g) => isMuted(g));
 
-				res.props.children.unshift(
-					<MenuGroup>
-						<MenuItem
-							disabled={mutedGuilds.length === currFolder.guildIds.length}
-							id="mute-folder"
-							label={Messages.MTF_MUTE_LABEL}
-							action={() => {
-								this.updateGuilds(unmutedGuilds);
-							}}
-						/>
-						<MenuItem
-							disabled={unmutedGuilds.length === currFolder.guildIds.length}
-							id="unmute-folder"
-							label={Messages.MTF_UNMUTE_LABEL}
-							action={() => this.updateGuilds(mutedGuilds, false)}
-						/>
-					</MenuGroup>
-				);
-
-				return res;
-			});
-
-			GuildFolderContextMenu.default.displayName = "GuildFolderContextMenu";
-		});
-	}
-
-	patchContextMenu(displayName, patch) {
-		const filter = (m) => m.default && m.default.displayName === displayName;
-		const m = getModule(filter, false);
-		if (m) patch(m);
-		else {
-			const module = getModule(["openContextMenuLazy"], false);
-			inject(
-				"mute-folder-context-menu-lazy",
-				module,
-				"openContextMenuLazy",
-				(args) => {
-					const lazyRender = args[1];
-					args[1] = async () => {
-						const render = await lazyRender(args[0]);
-
-						return (config) => {
-							const menu = render(config);
-							if (menu?.type?.displayName === displayName && patch) {
-								uninject("mute-folder-context-menu-lazy");
-								patch(getModule(filter, false));
-								patch = false;
-							}
-							return menu;
-						};
-					};
-					return args;
-				},
-				true
+			res.props.children.unshift(
+				<MenuGroup>
+					<MenuItem
+						disabled={mutedGuilds.length === currFolder.guildIds.length}
+						id="mute-folder"
+						label={Messages.MTF_MUTE_LABEL}
+						action={() => {
+							this.updateGuilds(unmutedGuilds);
+						}}
+					/>
+					<MenuItem
+						disabled={unmutedGuilds.length === currFolder.guildIds.length}
+						id="unmute-folder"
+						label={Messages.MTF_UNMUTE_LABEL}
+						action={() => this.updateGuilds(mutedGuilds, false)}
+					/>
+				</MenuGroup>
 			);
-		}
+
+			return res;
+		});
 	}
 
 	updateGuilds(guilds, bool = true) {
@@ -88,7 +54,6 @@ module.exports = class MuteFolder extends Plugin {
 	}
 
 	pluginWillUnload() {
-		uninject("mute-folder");
 		uninject("mute-folder-context-menu-lazy");
 	}
 };
